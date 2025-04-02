008b743ad4474832cc093f5778886908b90f64e8
TwiceDivorced <serhardt@mines.edu>
2025-04-02 15:27:27 -0600

    Removed water weight as constant

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,8 @@
 #include <string>
 #include <stack>
 
-const int POUNDS_PER_SACK = 94;
-const float PPG_WATER = 8.34;
 const float SG_BENTONITE = 2.65;
+const int POUNDS_PER_SACK = 94;              // lb
 const float WATER_PER_PERC_BENTONITE = 0.65; // gal
 
 int main()
@@ -15,7 +14,7 @@ int main()
     std::stack<float> masses;  // gal
 
     // user input for specific gravity and water-cement ratio
-    float specificGravity, waterCementRatio, percentBentonite;
+    float specificGravity, waterCementRatio, percentBentonite, ppgWater;
 
     std::cout << "Enter specific gravity of the cement: ";
     std::cin >> specificGravity;
@@ -23,24 +22,26 @@ int main()
     std::cin >> waterCementRatio;
     std::cout << "Enter percent bentonite as a percent (enter 3 for 3%): ";
     std::cin >> percentBentonite;
+    std::cout << "Enter weight of water in ppg: ";
+    std::cin >> ppgWater;
 
     // cement MV
     masses.push(POUNDS_PER_SACK);
-    volumes.push(POUNDS_PER_SACK / specificGravity / PPG_WATER);
+    volumes.push(POUNDS_PER_SACK / specificGravity / ppgWater);
 
     // water MV
-    float waterWeight = waterCementRatio * PPG_WATER;
+    float waterWeight = waterCementRatio * ppgWater;
     masses.push(waterWeight);
     volumes.push(waterCementRatio);
 
     // bentonite MV
     float bentoniteWeight = (percentBentonite / 100) * POUNDS_PER_SACK;
-    float bentoniteWaterWeight = PPG_WATER * percentBentonite * WATER_PER_PERC_BENTONITE;
+    float bentoniteWaterWeight = ppgWater * percentBentonite * WATER_PER_PERC_BENTONITE;
     masses.push(bentoniteWeight);
     masses.push(bentoniteWaterWeight);
 
-    float bentoniteVolume = bentoniteWeight / SG_BENTONITE / PPG_WATER;
-    float bentoniteWaterVolume = bentoniteWaterWeight / PPG_WATER;
+    float bentoniteVolume = bentoniteWeight / SG_BENTONITE / ppgWater;
+    float bentoniteWaterVolume = bentoniteWaterWeight / ppgWater;
     volumes.push(bentoniteVolume);
     volumes.push(bentoniteWaterVolume);
 
